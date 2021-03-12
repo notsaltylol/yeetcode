7eeba81a8bae7df9786fb0b078b8759443eb7835
Leon <kwan.leon@gmail.com>
2021-03-11 21:42:58 -0500

    lolxd

---
+++ 1copypasta.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+using ll = long long;
+using vi = vector<int>;
+#define pb push_back
+#define rsz resize
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+using pi = pair<int,int>;
+#define f first
+#define s second
+#define mp make_pair
+
+using namespace std;
+int main(){
+    pi pairOne = mp(1,2)
+    int firstElem = pairOne.f
+}
