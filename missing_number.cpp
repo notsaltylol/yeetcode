deee68a6cabb3ad4c8b807eae41a16064f0b5e9d
Leon <kwan.leon@gmail.com>
2021-03-11 22:59:01 -0500

    missing number

--- missing_number.cpp
+++ missing_number.cpp
@@ -14,12 +14,19 @@ using pi = pair<int,int>;
 int main(){
     string input;
     cin>>input;
-    long long n = stoll(input);
-    cout<<n<<" ";
-    while(n!=1){
-        if(n%2==0)n/=2;
-        else n=3*n+1; 
-        cout<<n<<" ";
+    vector<bool> b;
+    int n = stoll(input);
+    for(int i=0;i<n;i++){
+        b.push_back(false);
+    }
+    for(int i=0;i<n-1;i++){
+        cin >> input;
+        b[stoi(input)-1] = true;
+    }
+    for(int i=0;i<n;i++){
+        if(!b[i]){
+            cout<<i+1;
+        }
     }
     return 0;
 }
