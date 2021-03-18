b5255b5c79b603c78b46ed09ad8a4688bd1e8825
Leon <kwan.leon@gmail.com>
2021-03-17 22:08:57 -0400

    .

--- ferris_wheel.cpp
+++ ferris_wheel.cpp
@@ -11,21 +11,29 @@ using pi = pair<int,int>;
 #define s second
 #define mp make_pair
 #define rep(i,a,b) for (int i = a; i < b; i++)
-
-
-ll fact(int n){
-    int x=1;
-    rep(i,0,n){
-        x*=i+1;
-    }
-    return x;
-}
-
+    
 int main(){
-    pi pairOne = mp(1,2);
-    int firstElem = pairOne.f;
-    cout << firstElem << endl;
-    string input;
-    cin>>input;
-    long long n = stoll(input);
+    int n,x,in;
+    cin>>n>>x;
+    vi p;
+    rep(i,0,n){
+        cin>>in;
+        p.pb(in);
+    }
+    int i=0;
+    int j=sz(p)-1;
+    int g=0;
+    sort(all(p));
+    while(i<=j){
+        if(p[i]+p[j]<=x){
+            g++;
+            i++;
+            j--;
+        }else{
+            g++;
+            j--;
+        }
+    }
+    cout<<g;
+    return 0;
 }
